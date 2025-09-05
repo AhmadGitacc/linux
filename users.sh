c17300cfbdefa51ee689ef6787f391c1fe4b34ee
Ahmad Dangana <danganaahmed5@gmail.com>
2025-09-05 12:33:00 +0100

    update files

--- users.sh
+++ users.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 GROUP="devteam"
-USERS=("userA" "userB" "userC" "userD" "userE")
+USERS=("user1" "user2" "user3" "user4" "user5")
 
 # Create group if not exists
 if ! getent group $GROUP >/dev/null; then
@@ -17,8 +17,9 @@ for USER in "${USERS[@]}"; do
     echo "User $USER already exists."
   else
     sudo useradd -m -s /bin/bash -g $GROUP $USER
-    echo "$USER:Password123" | sudo chpasswd
+    echo "$USER:Password" | sudo chpasswd
     sudo chage -d 0 $USER   # force password reset at next login
     echo "User $USER created and added to $GROUP."
   fi
 done
+ 
